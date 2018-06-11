bd287e52867c6f638c563db185671951f078b888
CaritoDasilva <caritodasilva@gmail.com>
2018-06-11 13:51:11 -0400

    Avances del data dasboard

---
+++ src/data.js
@@ -0,0 +1,15 @@
+window.computeUsersStats = (users, progress, courses) => {
+
+};
+
+window.sortUsers = (users, orderB, orderDirection) => {
+
+};
+
+window.filterUsers = (users, search) => {
+
+};
+
+window.processCohortData = (options) => {
+
+};
